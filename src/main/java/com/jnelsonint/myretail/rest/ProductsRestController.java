2ed7e4a505538f577c8d092dfa4e4f090e543205
Jeff Nelson <nels4784@gmail.com>
2021-05-25 20:17:17 -0500

    return 404 when nothing found

--- src/main/java/com/jnelsonint/myretail/rest/ProductsRestController.java
+++ src/main/java/com/jnelsonint/myretail/rest/ProductsRestController.java
@@ -1,5 +1,6 @@
 package com.jnelsonint.myretail.rest;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -20,13 +21,17 @@ public class ProductsRestController {
   private final ProductProvider provider;
 
   @GetMapping("/products/{id}")
-  public Mono<ProductDTO> getProductById(@PathVariable Integer id) {
-    return provider.getById(id);
+  public Mono<ResponseEntity<ProductDTO>> getProductById(@PathVariable Integer id) {
+    return provider.getById(id)
+        .map(product -> ResponseEntity.ok(product))
+        .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
   }
 
   @PutMapping(value = "/products/{id}", consumes = "application/json")
-  public Mono<ProductDTO> updatePricing(@PathVariable Integer id, @RequestBody ProductUpdateDTO update) {
-    return provider.updatePricing(id, update.getUpdatedPrice().getValue());
+  public Mono<ResponseEntity<ProductDTO>> updatePricing(@PathVariable Integer id, @RequestBody ProductUpdateDTO update) {
+    return provider.updatePricing(id, update.getUpdatedPrice().getValue())
+        .map(product -> ResponseEntity.ok(product))
+        .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
   }
 
 }
