2e30a09a8a357510f365350aaf9a45c63df63b6b
Jeff Nelson <nels4784@gmail.com>
2021-05-25 19:35:19 -0500

    use sample product ids given

--- src/main/java/com/jnelsonint/myretail/domain/DefaultDataContributor.java
+++ src/main/java/com/jnelsonint/myretail/domain/DefaultDataContributor.java
@@ -1,6 +1,8 @@
 package com.jnelsonint.myretail.domain;
 
 import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -18,13 +20,12 @@ public class DefaultDataContributor implements ApplicationListener<ContextRefres
   @Override
   public void onApplicationEvent(ContextRefreshedEvent event) {
     BigDecimal price = BigDecimal.valueOf(123.45);
-    for (Integer productId = 13860427; productId < 13860429; productId++) {
-      repo.save(new ProductPricing()
-          .setProductId(productId)
-          .setPrice(price))
-          .block();
-      price = price.add(BigDecimal.valueOf(1));
-    }
+    AtomicInteger priceIncrementor = new AtomicInteger(1);
+    Stream.of(13860427, 54456119, 13264003, 12954218)
+        .forEach(productId -> repo.save(new ProductPricing()
+            .setProductId(productId)
+            .setPrice(price.add(BigDecimal.valueOf(priceIncrementor.getAndIncrement()))))
+            .block());
   }
 
 }
